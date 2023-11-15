5b0f3bc648e935f43f733374970b21c31cb9c467
Synxx12 <emibnu9@gmail.com>
2023-11-15 16:05:14 +0700

    awdaWD

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.send({
     message: "Welcome To JKT48 WEB API",
     author: "https://github.com/Synxx12",
-    repository: "https://github.com/ikhbaldwiyan/jkt48-showroom-api",
+    repository: "https://github.com/Synxx12/JKT48-Scrape.git",
   });
 });
 
