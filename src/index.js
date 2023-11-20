cd82a8101db502bbe58bdfc576c7dd066fe38461
Synxx12 <emibnu9@gmail.com>
2023-11-20 13:53:52 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes/routes");
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
