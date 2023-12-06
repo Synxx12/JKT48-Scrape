25d68a6b2ecf352f04c0d1d81b4e6297c8d1b2a1
Synxx12 <emibnu9@gmail.com>
2023-12-06 15:48:29 +0700

    false maintenanice

--- src/main/config.js
+++ src/main/config.js
@@ -3,6 +3,6 @@ module.exports = {
   discordWebhookUrl: "https://discord.com/api/webhooks/1181867082049134602/8wfuaOPUpP4XMxENcSvyrSk2wAIHUBKgiZvTLmvtkc5_dm-OmRL304VFlhRqdzQ4SiKx",
   botAvatarUrl:
     "https://cdn.discordapp.com/attachments/1115168261991055412/1181868931582017617/Snapinsta.app_366417511_166937156417495_2535731357802332395_n_1080.jpg?ex=6582a037&is=65702b37&hm=bff179b3f791268b30235a835022d5b0634188a62aadab368f4ad52a5ce9325d&",
-  maintenanceMode: true,
+  maintenanceMode: false,
   port: 3000,
 };
